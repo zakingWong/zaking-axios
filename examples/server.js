64c9117b3e33bb435caa0a26956d9854ea9490ee
wangjiaqi <521128056@qq.com>
2022-02-24 10:14:47 +0800

    feat: add notes

--- examples/server.js
+++ examples/server.js
@@ -7,6 +7,7 @@ const WebpackConfig = require("./webpack.config");
 
 const app = express();
 const compiler = webpack(WebpackConfig);
+// 使用node的中间件来读取webpack配置进行server的打包
 app.use(
   webpackDevMiddleware(compiler, {
     publicPath: "/__build__/",
@@ -21,15 +22,19 @@ app.use(webpackHotMiddleware(compiler));
 app.use(express.static(__dirname));
 const router = express.Router();
 
+// 注册路由
 registerC1Router();
-
+// 使用路由
 app.use(router);
 
+// 端口号
 const port = process.env.PORT || 9090;
+// 启动服务
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`);
 });
 
+// 注册get方法的路由
 function registerC1Router() {
   router.get("/c1/get", function (req, res) {
     res.json({
