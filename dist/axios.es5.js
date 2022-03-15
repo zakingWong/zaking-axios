8a788dd8acf81711ee6e87a3b7c9834f1a53979c
wangjiaqi <521128056@qq.com>
2022-03-15 14:06:55 +0800

    feat: add simple demo

--- dist/axios.es5.js
+++ dist/axios.es5.js
@@ -1,5 +1,13 @@
 function axios() {
-  console.log("axios");
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://httpbin.org/post");
+  xhr.send(JSON.stringify({ a: 1, b: 2 }));
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      console.log(xhr.response);
+      console.log(xhr.responseText);
+    }
+  };
 }
 
 export { axios as default };
